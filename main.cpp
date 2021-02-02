9c1cc7b5d2dfe211300e664b718e3f9a0631e8a2
Hassan Ayad <hayad@gmail.com>
2021-02-02 16:18:49 +0200

    namespace std used

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 int main(){
-	std::cout<<"Test"<<std::endl;
-     std::cout<<"Hello World"<<std::endl;
-     std::cout<<"New Commit"<<std::endl;
+	cout<<"Test"<<endl;
+     cout<<"Hello World"<<endl;
+     cout<<"New Commit"<<endl;
 
 }
